8ed83bd0d5d6479638956c3b61fc21352f196709
Park0720 <ppmm98@naver.com>
2023-03-02 09:26:39 +0900

    1단계

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("명령) ");
+        String order = sc.nextLine();
+
+    }
+}
