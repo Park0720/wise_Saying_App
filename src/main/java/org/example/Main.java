b2bcc1de401d0a4f1065d85f5f4995e97964a559
Park0720 <ppmm98@naver.com>
2023-03-02 11:35:05 +0900

    8ë8단계(수정)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,6 @@ public class Main {
             if (order.equals("종료")) {
                 break;
             }
-
             else if (order.equals("등록")) {
                 long id = lastWiseSaying + 1;
                 System.out.printf("명언 : ");
@@ -29,31 +28,35 @@ public class Main {
                 lastWiseSaying = id;
             }
             else if (order.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언\n"+"-".repeat(30));
-                for(int i = wise.size()-1; i>=0;i--){
-                    System.out.println(wise.get(i).getId() + " / " + wise.get(i).getAuther()+ " / " + wise.get(i).getWise_Saying());;
+                System.out.println("번호 / 작가 / 명언\n" + "-".repeat(30));
+                for (int i = wise.size() - 1; i >= 0; i--) {
+                    System.out.println(wise.get(i).getId() + " / " + wise.get(i).getAuther() + " / " + wise.get(i).getWise_Saying());
+                    ;
                 }
             }
-            else if(order.equals("삭제?id=1")){
-                if (wise.get(0).getId() == 1){
-                    System.out.println(wise.get(0).getId()+"번 명언이 삭제 되었습니다.");
-                    wise.remove(0);
+            else if (order.equals("삭제?id=1")) {
+                for (int i = 0; i < wise.size(); i++) {
+                    if (wise.get(i).getId() == 1) {
+                        System.out.println(wise.get(i).getId() + "번 명언이 삭제 되었습니다.");
+                        wise.remove(i);
 
-                }
-                else{
-                    System.out.println("1번 명언은 존재하지 않습니다.");
+                    } else {
+                        System.out.println("1번 명언은 존재하지 않습니다.");
+                    }
                 }
             }
-            else if(order.equals("수정?id=2")){
-                if(wise.get(0).getId()==2) {
-                    System.out.println("명언(기존) : " + wise.get(0).getWise_Saying());
-                    System.out.printf("명언 : ");
-                    String wise_Saying = sc.nextLine().trim();
-                    System.out.println("작가(기존) : " + wise.get(0).getAuther());
-                    System.out.printf("작가 : ");
-                    String auther = sc.nextLine().trim();
-                    wise.get(0).setWise_Saying(wise_Saying);
-                    wise.get(0).setAuther(auther);
+            else if (order.equals("수정?id=2")) {
+                for (int i = 0; i < wise.size(); i++) {
+                    if (wise.get(i).getId() == 2) {
+                        System.out.println("명언(기존) : " + wise.get(i).getWise_Saying());
+                        System.out.printf("명언 : ");
+                        String wise_Saying = sc.nextLine().trim();
+                        System.out.println("작가(기존) : " + wise.get(i).getAuther());
+                        System.out.printf("작가 : ");
+                        String auther = sc.nextLine().trim();
+                        wise.get(i).setWise_Saying(wise_Saying);
+                        wise.get(i).setAuther(auther);
+                    }
                 }
             }
         }
