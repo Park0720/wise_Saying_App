67ed5c8245e7b4aac532cbc74cef6036ec5f5e47
Park0720 <ppmm98@naver.com>
2023-03-02 09:39:27 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
         Scanner sc = new Scanner(System.in);
-        System.out.printf("명령) ");
-        String order = sc.nextLine();
-
+        while (true) {
+            System.out.printf("명령) ");
+            String order = sc.nextLine();
+            if(order.equals("종료")) {
+                break;
+            }
+            if(order.equals("등록"));{
+                System.out.printf("명언 : ");
+                String wise_Saying = sc.nextLine();
+                System.out.printf("작가 : ");
+                String auther = sc.nextLine();
+            }
+        }
     }
 }
