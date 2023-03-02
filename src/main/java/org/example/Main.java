94251a40380d2f3451223ca7c5f08922b54dfa26
Park0720 <ppmm98@naver.com>
2023-03-02 10:38:11 +0900

    5단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,63 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-        int i=1;
+        long lastWiseSaying = 0;
+        List<Wise> wise = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.printf("명령) ");
-            String order = sc.nextLine();
-            if(order.equals("종료")) {
+            String order = sc.nextLine().trim();
+            if (order.equals("종료")) {
                 break;
             }
-            if(order.equals("등록"));{
+
+            else if (order.equals("등록")) {
+                long id = lastWiseSaying + 1;
                 System.out.printf("명언 : ");
-                String wise_Saying = sc.nextLine();
+                String wise_Saying = sc.nextLine().trim();
                 System.out.printf("작가 : ");
-                String auther = sc.nextLine();
-                Wise wise_saying = new Wise(wise_Saying,auther);
-                System.out.println( i +"번 명언이 등록되었습니다.");
-                i++;
+                String auther = sc.nextLine().trim();
+                Wise wise_saying = new Wise(id, wise_Saying, auther);
+                wise.add(wise_saying);
+                System.out.println(id + "번 명언이 등록되었습니다.");
+                lastWiseSaying = id;
+            }
+            else if (order.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언\n------------------------");
+                for(int i = wise.size()-1; i>=0;i--){
+                    System.out.println(wise.get(i).getId() + " / " + wise.get(i).getAuther()+ " / " + wise.get(i).getWise_Saying());;
+                }
             }
         }
     }
 }
-class Wise{
-    String wise_Saying;
-    String auther;
-    Wise(String wise_Saying, String auther){
-        this.wise_Saying=wise_Saying;
-        this.auther=auther;
+
+class Wise {
+    private long id;
+    private String wise_Saying;
+    private String auther;
+
+    public Wise(long id, String wise_Saying, String auther) {
+        this.id = id;
+        this.wise_Saying = wise_Saying;
+        this.auther = auther;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getWise_Saying() {
+        return wise_Saying;
+    }
+
+    public String getAuther() {
+        return auther;
     }
 }
