6847c3bb07be08b1f7a846cf5419f9a5da4a336f
Park0720 <ppmm98@naver.com>
2023-03-02 09:49:04 +0900

    4단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
+        int i=1;
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.printf("명령) ");
@@ -17,8 +18,18 @@ public class Main {
                 String wise_Saying = sc.nextLine();
                 System.out.printf("작가 : ");
                 String auther = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                Wise wise_saying = new Wise(wise_Saying,auther);
+                System.out.println( i +"번 명언이 등록되었습니다.");
+                i++;
             }
         }
+    }
+}
+class Wise{
+    String wise_Saying;
+    String auther;
+    Wise(String wise_Saying, String auther){
+        this.wise_Saying=wise_Saying;
+        this.auther=auther;
     }
 }
